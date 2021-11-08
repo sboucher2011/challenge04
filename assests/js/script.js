339f247cc4b4b78d6ad8e91477da95c8e99d8865
sboucher <sboucher2011@gmail.com>
2021-11-08 14:05:23 -0500

    added real questions and updated timer to subtract

--- assests/js/script.js
+++ assests/js/script.js
@@ -9,13 +9,60 @@ var headerEl = document.querySelector("#quizTitle");
 var directionsEl = document.querySelector(".directions");
 var quizContentEl = document.querySelector("#quizContent");
 var quizSetupEl = document.querySelector(".quizSetup");
-var timeLeft = 15;
+var timeLeft = 60;
 
+//Create Quiz Header
+var headerQuiz = document.createElement("h1");
+headerQuiz.className = "quizH1";
+quizContentEl.appendChild(headerQuiz);
+
+//Create Unordered List
+var listEl = document.createElement("ul"); 
+var bttn1 = document.createElement("li");
+var bttn2 = document.createElement("li");
+var bttn3 = document.createElement("li");
+var bttn4 = document.createElement("li");
+  
+//create buttons
+var buttonOne = document.createElement("button");
+buttonOne.textContent = "Initial Name";
+buttonOne.className = "button";
+buttonOne.id = "A";
+bttn1.appendChild(buttonOne);
+
+var buttonTwo = document.createElement("button");
+buttonTwo.textContent = "Initial Name 2";
+buttonTwo.className = "button";
+buttonTwo.id = "B";
+bttn2.appendChild(buttonTwo);
+
+var buttonThree = document.createElement("button");
+buttonThree.textContent = "Initial Name 2";
+buttonThree.className = "button";
+buttonThree.id = "C";
+bttn3.appendChild(buttonThree);
+
+var buttonFour = document.createElement("button");
+buttonFour.textContent = "Initial Name 2";
+buttonFour.className = "button";
+buttonFour.id = "D";
+bttn4.appendChild(buttonFour);
+
+var answerDisplay = document.createElement("h3");
+
+//Question Setup 
+var i = 0;
 
 var questions = [
   //[Question, Option 1, Option 2, Option 3, Option 4, Solution]
-  ["What is the question?", "A", "B", "C", "D", "#B"],
-  ["What is the next question?", "A", "B", "C", "D", "#A"],
+  ["Which of the following is true about variable naming conventions?", "1. Must begin with a letter or underscore charracter", "2. case sensitive", "3. can start with number or letter", "4. None of the Above", "#A"],
+  ["Which built-in method adds one or more elements to the end of an array and returns the new length of the array?", "1. last()", "2. put()", "3. push()", "4. None of the Above", "#C"],
+  ["Which of the following function of String object combines the text of two strings and returns a new string?", "1. add()", "2. merge()", "3. concat()", "4. append()", "#C"],
+  ["Array's can be used to store?", "1. numbers and strings", "2. other arrays", "3. booleans", "4. All of the Above", "#D"],
+  ["String values must be enclosed within _____ when being assigned to variables?", "1. commas", "2. curly brackets", "3. quotes", "4. parenthesis", "#C"],
+  ["A very useful tool used during development and debugging for printing content to the debugger is:", "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log", "#D"],
+  ["The condition in an if/else statement is enclosed with _____", "1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets", "#B"],
+  ["Commonly used data types DO Not Include:", "1. strings", "2. booleans", "3. alerts", "4. numbers", "#C"],
 ];
 
 //---------------------------------------------------------------------
@@ -63,44 +110,6 @@ function displayQuiz() {
   //remove statement
   directionsEl.style.display = "none";
 
-  //Create Quiz Header
-  var headerQuiz = document.createElement("h1");
-  headerQuiz.className = "quizH1";
-  quizContentEl.appendChild(headerQuiz);
-
-  //Create Unordered List
-  var listEl = document.createElement("ul"); 
-  var bttn1 = document.createElement("li");
-  var bttn2 = document.createElement("li");
-  var bttn3 = document.createElement("li");
-  var bttn4 = document.createElement("li");
-  
-  //create buttons
-  var buttonOne = document.createElement("button");
-  buttonOne.textContent = "Initial Name";
-  buttonOne.className = "button";
-  buttonOne.id = "A";
-
-  bttn1.appendChild(buttonOne);
-
-  var buttonTwo = document.createElement("button");
-  buttonTwo.textContent = "Initial Name 2";
-  buttonTwo.className = "button";
-  buttonTwo.id = "B";
-  bttn2.appendChild(buttonTwo);
-
-  var buttonThree = document.createElement("button");
-  buttonThree.textContent = "Initial Name 2";
-  buttonThree.className = "button";
-  buttonThree.id = "C";
-  bttn3.appendChild(buttonThree);
-
-  var buttonFour = document.createElement("button");
-  buttonFour.textContent = "Initial Name 2";
-  buttonFour.className = "button";
-  buttonFour.id = "D";
-  bttn4.appendChild(buttonFour);
-
   //Add to HTML
   body.appendChild(listEl);
 
@@ -114,41 +123,56 @@ function displayQuiz() {
   bttn3.setAttribute("style", " color:white;");
   bttn4.setAttribute("style", " color:white;");
 
-  //Data Property for which button is clicked
+  //Display First Question
+  displayQuestion();
 
-  for(let i = 0; i < questions.length; i++) {
-    //Display Header
+  //check ID of button to see if answer was correct
+  body.addEventListener("click", function(event) {
+    var element = event.target;
+
+    if (element.matches(questions[0][5])) {
+      console.log("Match!");
+      i++;
+      displayQuestion();
+      displayAnswerStatus("Correct!");
+          
+    } else if (element.matches("#A") || element.matches("#B") || element.matches("#C") || element.matches("#D")) {
+      console.log("NOPE");
+      i++;
+      timeLeft = timeLeft - 10;
+      displayQuestion();
+      displayAnswerStatus("Wrong!");
+    }
+      
+  });
+
+}
+
+//Display Question
+function displayQuestion() {
+
+  if (i < questions.length) {
+    //Display Question
     headerQuiz.textContent = questions[i][0];
-
-
-    //Update Buttons
+  
+    //Update Buttons to Answers
     buttonOne.textContent = questions[i][1];
     buttonTwo.textContent = questions[i][2];
     buttonThree.textContent = questions[i][3];
     buttonFour.textContent = questions[i][4];
-
-    body.addEventListener("click", function(event) {
-      var element = event.target;
-
-      if (element.matches(questions[i][5])) {
-        console.log("Match!")
-        displayAnswerStatus("Correct!");
-      } else {
-        console.log("NOPE")
-        //displayAnswerStatus("Wrong!");
-      }
-      
-    });
-    
-      //switch to next question
-
-
+  } else {
+    console.log("end!!")
   }
+  
 }
 
 //Display Label After Answer is Selected
 function displayAnswerStatus(answer) {
-  var answerDisplay = document.createElement("h3");
+  
   answerDisplay.textContent = answer;
-  body.appendChild(answerDisplay);
+
+  if (i===1) {
+    body.appendChild(answerDisplay);
+  }
+  
 }
